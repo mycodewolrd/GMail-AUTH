33cafb68172eafee02a2a7e4125e6d6de67997dd
Rajnandini <rajnandinipatra2003@gmail.com>
2024-12-20 20:28:22 +0530

    db Config

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+
+export async function connect() {
+    try {
+        mongoose.connect(process.env.MONGODB_URI!)
+        const connection = mongoose.connection
+
+        connection.on("connection", () => {
+            console.log("MongoDB connected");
+        })
+
+        connection.on("error", (err) => {
+            console.log("MongoDB connection error, please make sure db is append running " + err);
+            process.exit()
+        })
+
+    } catch (error) {
+        console.log("Something went wrong in connecting DB");
+        console.log(error);
+        
+    }
+}
